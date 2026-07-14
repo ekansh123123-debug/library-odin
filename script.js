df4729d7eef077667cc284b1df292c5bec6ac44c
ekansh-debug <ekansh123123@gmail.com>
2026-07-14 13:21:17 +0530

    add to display function

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let myLibraray = [];
+let myLibraray = [
+    {
+        author: "ram",
+        titel: "astro",
+        info:"About astroname"
+    },
+    {
+        author: "shame",
+        titel: "non astro",
+        info: "agaist astro"
+    },
+    {
+        author:"anu",
+        titel: "another",
+        info: "ajdf"
+    }
+];
 
-function Book(title,author) {
+function Book(title,author,info) {
     this.id = crypto.randomUUID();
     this.titel = title;
     this.author = author;
+    this.info = info;
 }
 
 function addToLibrary(book) {
@@ -12,10 +29,31 @@ function addToLibrary(book) {
 
 const newBookButtonEvent = (e) => {
     e.preventDefault();
-    const newBook = document.createElement("div")
+    addBookToDisplay();
 }
+const addBookToDisplay = () => {
+    author.textContent = "";
+    info.textContent = "";
+    titel.textContent = "";
 
+    for (const book of myLibraray){
+        const newauthor = document.createElement("div");
+        const newinfo = document.createElement("div");
+        const newtitel = document.createElement("div");
+        
+        newauthor.textContent = book.author;
+        newinfo.textContent =  book.info;
+        newtitel.textContent = book.titel;
+        
+        author.appendChild(newauthor);
+        info.appendChild(newinfo);
+        titel.appendChild(newtitel);
+    }
+}
 const book = document.querySelector("#book");
 const newBookButton = document.querySelector("#newBookButton");
+const author = document.querySelector("#author");
+const info = document.querySelector("#info");
+const titel = document.querySelector("#titel");
 
 newBookButton.addEventListener("click",newBookButtonEvent);
